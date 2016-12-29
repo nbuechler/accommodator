f80cf67ff533a64893c005a9abf6a216e5aa69b3
nbuechler <natebuechler@gmail.com>
2016-12-28 23:36:07 -0500

    finished removing authentication and redirecting it to rely on fixed gateway

--- controllers/user.js
+++ controllers/user.js
@@ -27,9 +27,29 @@ exports.requiresLogin = function(req, res, next) {
  * Log out.
  */
 exports.postRemoteLogout = function(req, res) {
-	// TODO: Refactor this method to redirect authentication to fixed-gateway at port 9000
-  req.logout();
-  res.send({ customCode: 2001, status: 'Success', msg: 'Success! You are logged out.' });
+	// This method redirect authentication to fixed-gateway at port 9000
+
+	//postRemoteLogin (remote to :9000)
+	var options = {
+		// headers:{
+		// 	"X-My-Header": "This is a custom header field"
+		// },
+		method: 'post',
+	};
+
+	var email = req.query.email
+	var password = req.query.password
+
+	fetchUrl('http://' + config.ip + ':' + config.gatewayPort + '/postRemoteLogout',
+		options,
+		function(error, meta, body){
+			if (error) {
+				res.status(400).send(error);
+			} else {
+				res.send('OK');
+			}
+	});
+
 };
 
 /**
@@ -37,38 +57,32 @@ exports.postRemoteLogout = function(req, res) {
  * Create a new local account.
  */
 exports.postRemoteSignup = function(req, res, next) {
-	// TODO: Refactor this method to redirect authentication to fixed-gateway at port 9000
-  req.assert('email', 'Email is not valid').isEmail();
-  req.assert('password', 'Password must be at least 4 characters long').len(4);
-  req.assert('confirmPassword', 'Passwords do not match').equals(req.assert('password').value);
+	// This method redirect authentication to fixed-gateway at port 9000
 
-  var errors = req.validationErrors();
+		//postRemoteLogin (remote to :9000)
+		var options = {
+			// headers:{
+			// 	"X-My-Header": "This is a custom header field"
+			// },
+			method: 'post',
+			payload: '{}'
+		};
 
-  if (errors) {
-    req.flash('errors', errors);
-    return res.send({ customCode: 4031, status: 'Failure', errors: errors })
-  }
+		var email = req.query.email
+		var password = req.query.password
+	  var confirmPassword = req.query.confirmPassword;
 
-  var user = new User({
-    email: req.assert('email').value,
-    password: req.assert('password').value
-  });
+		fetchUrl('http://' + config.ip + ':' + config.gatewayPort + '/postRemoteSignup' + '/?email=' + email + '&password=' + password + '&confirmPassword=' + confirmPassword,
+			options,
+			function(error, meta, body){
+				if (error) {
+					res.status(400).send(error);
+				} else {
+					res.send(body.toString());
+				}
 
-console.log(req.assert('email').value);
+		});
 
-  User.findOne({ email: req.assert('email').value }, function(err, existingUser) {
-    if (existingUser) {
-      req.flash('errors', { msg: 'Account with that email address already exists.' });
-      return res.send({ customCode: 4032, status: 'Failure', msg: 'Account with that email address already exists.' })
-    }
-    user.save(function(err) {
-      if (err) return next(err);
-      req.logIn(user, function(err) {
-        if (err) return next(err);
-        return res.send({ _id: user._id, customCode: 2001, status: 'Success', msg: 'Success! You have a new account, and you logged in.' })
-      });
-    });
-  });
 };
 
 /**
@@ -76,6 +90,7 @@ console.log(req.assert('email').value);
  * Sign in using email and password.
  */
 exports.postRemoteLogin = function(req, res, next) {
+	// This method redirect authentication to fixed-gateway at port 9000
 
 	//postRemoteLogin (remote to :9000)
 	var options = {
